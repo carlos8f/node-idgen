a3dff14d6252887683d4a6d875ec1df00ad3037d
Carlos Rodriguez <carlos@s8f.org>
2013-06-20 10:29:29 -0700

    update tests for node 0.10

--- test/idgen.js
+++ test/idgen.js
@@ -2,24 +2,16 @@ var idgen = require('../')
   , assert = require('assert')
   ;
 
-describe('idgen', function() {
-  it('creates unique IDs', function(done) {
+describe('idgen', function () {
+  it('creates unique IDs', function () {
     var ids = [], id;
-    function nextTest() {
+    for (var i = 0; i < 5000; i++) {
       id = idgen(16);
       assert.strictEqual(typeof id, 'string', 'id is string');
       assert.strictEqual(id.indexOf('undefined'), -1, 'no "undefined" in id');
       assert.strictEqual(ids.indexOf(id), -1, 'id is unique');
       assert.strictEqual(id.length, 16, 'id is custom length');
       ids.push(id);
-
-      if (ids.length === 5000) {
-        done();
-      }
-      else {
-        process.nextTick(nextTest);
-      }
     }
-    nextTest();
   });
 });
