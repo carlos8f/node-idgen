5fe7a6b6d4f30920f5c77a09fc47d45a814be39d
Carlos Rodriguez <carlos@s8f.org>
2014-04-06 00:27:02 -0700

    allow setting encoding on CLI

--- bin/idgen.sh
+++ bin/idgen.sh
@@ -2,17 +2,24 @@
 
 var buf = Buffer('')
   , idgen = require('../')
+  , cmd = require('commander')
+
+cmd
+  .version(require('../package.json').version)
+  .description(require('../package.json').description)
+  .option('-e, --encoding <encoding>', 'specify encoding for STDIN')
+  .parse(process.argv)
 
 process.stdin.on('readable', function () {
   var chunk = process.stdin.read();
   if (chunk !== null) {
-    buf = Buffer.concat([buf, chunk]);
+    buf = Buffer.concat([buf, cmd.encoding ? Buffer(String(chunk), cmd.encoding) : chunk]);
   }
   else {
     process.stdin.pause();
     var len = process.argv[2] ? parseInt(process.argv[2], 10) : null;
     buf = buf.length ? buf : null;
-    console.log(idgen(len, buf, true));
+    console.log(idgen(len, buf));
     process.exit();
   }
 });
