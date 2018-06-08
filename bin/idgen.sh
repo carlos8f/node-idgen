b17e2e6353f3ddf417f2dc44817e96d526756996
Carlos Rodriguez <carlos@s8f.org>
2018-06-08 12:59:38 -0700

    fix

--- bin/idgen.sh
+++ bin/idgen.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env node
-
-var buf = Buffer('')
-  , idgen = require('../')
-
-process.stdin.on('readable', function () {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    buf = Buffer.concat([buf, chunk]);
-  }
-  else {
-    process.stdin.pause();
-    var len = process.argv[2] ? parseInt(process.argv[2], 10) : null;
-    buf = buf.length ? buf : null;
-    console.log(idgen(len, buf, true));
-    process.exit();
-  }
-});
+var idgen = require('../')
+var len = process.argv[2] ? parseInt(process.argv[2], 10) : null;
+console.log(idgen(len))
